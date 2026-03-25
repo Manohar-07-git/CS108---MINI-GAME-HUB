b00f396a90e0982855aa3702b7214db2257030c5
manohar <manohartalabattula7@gmail.com>
2026-03-25 15:16:40 +0000

    merged both

--- main.sh
+++ main.sh
@@ -1,60 +1,61 @@
 #!/bin/bash
-[[ ! -e users.tsv ]] && touch users.tsv
-cond(){
-	authentication
-	username1=$username
-	authentication
-	username2=$username
-	if [[ "$username2" != "$username1" ]];then
-        python3 game.py $username1 $username2
-else
-	echo "both usernames cannot be same enter user2 again"
-		authentication
-       username2=$username
-       cond
-	fi
+
+FILE="users.tsv"
+[[ ! -f "$FILE" ]] && touch "$FILE"
+
+hash_fun(){
+    echo -n "$1" | sha256sum | awk '{print $1}'
 }
+
 register(){
-        echo "user do not exist  do you want to register(yes/no)?"
-        read -r response
-        if [[ "$response" == "yes" ]]; then
-                echo "set a password: "
-                read -r -s passwordn
-                echo -e "$username\t$(echo -n "$passwordn" | sha256sum | awk '{print $1}')" >> users.tsv
-                echo "updated user"
-        elif [[ "$response" == "no" ]];then
-         authentication
-        else
-                echo "input either yes or no"
-                register
-        fi
-	}
+    echo "user do not exist do you want to register(yes/no)?"
+    read -r response
+    if [[ "$response" == "yes" ]]; then
+        echo "set a password: "
+        read -r -s passwordn
+        echo -e "$username\t$(hash_fun "$passwordn")" >> "$FILE"
+        echo "updated user"
+    elif [[ "$response" == "no" ]]; then
+        authentication
+    else
+        echo "input either yes or no"
+        register
+    fi
+}
 
 authentication(){
-echo "enter username: "
-read -r username
-
-if [[ "$(awk -F "\t" -v user="$username" ' $1 == user {print $1}' users.tsv)" == "$username" ]];then
-	hpass=$(awk -F "\t" -v user="$username" ' $1 == user {print $2}' users.tsv)
-	echo "enter password: "
-        read -r -s password
-	if [[ "$( echo -n "$password" | sha256sum | awk '{print $1}' )" == "$hpass" ]];then
-		echo "user authenticated"
-	else 
-			echo "incorrect password"
-		authentication
-	fi
-else
-	register
-fi
+    echo "enter username: "
+    read -r username
+    stored_hash=$(awk -F "\t" -v user="$username" '$1 == user {print $2}' "$FILE")
+    if [[ -n "$stored_hash" ]]; then
+        while true; do
+            echo "enter password: "
+            read -r -s password
+            if [[ "$(hash_fun "$password")" == "$stored_hash" ]]; then
+                echo "user authenticated"
+                break
+            else
+                echo "incorrect password"
+            fi
+        done
+    else
+        register
+        authentication
+    fi
 }
+
+cond(){
+    authentication
+    username1=$username
+    authentication
+    username2=$username
+    if [[ "$username2" == "$username1" ]]; then
+        echo "both usernames cannot be same enter user2 again"
+        authentication
+        username2=$username
+        cond
+    fi
+    python3 game.py "$username1" "$username2"
+}
+
 cond
-	
-
-
-
-
-
-        
-
-
