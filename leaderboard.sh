288e39865a390022519681276e4772de340fff62
sathwik <as.28052008@gmail.com>
2026-04-02 00:12:35 +0530

    leaderboard.sh with some bugs fixed

--- leaderboard.sh
+++ leaderboard.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 stat=$1
+> leaderboard.txt
 n=$(wc -l users.tsv | awk 'print{$1}')
-for k in {tic-tac-toe,othello,connect4}
+for k in tic-tac-toe othello connect4
 do
 echo "$k"
-for i in `seq 1 $n`
+> leaderboard.txt
+for i in $(seq 1 $n)
 do
 	a=$(awk 'NR==$i {print $1}' users.tsv)
-        wins=$(awk -F ',' -v ka=$k '$4==$ka print{$1}' history.csv | grep -c "$a")
-	loss=$(awk -F ',' -v ka=$k '$4==$ka print{$3}' history.csv | grep -c "$a")
-       if [[ "$loss"="0" ]]
-	       wlratio="$wins"
+        wins=$(awk -F ',' -v ka="$k" '$4==ka {print $1}' history.csv | grep -wc "$a")
+	loss=$(awk -F ',' -v ka="$k" '$4==ka {print $3}' history.csv | grep -wc "$a")
+	if [[ "$loss" == "0" ]]; then
+		wlratio="$wins"
        else
 	       wlratio=$(( $wins/$loss ))
        fi
 	echo "$a,$wins,$loss,$wlratio" >> leaderboard.txt
 done
-if [[ "$stat"=="wins" ]]; then
-   sort -t ',' -k2,2 leaderboard.txt
+if [[ "$stat" == "wins" ]]; then
+   sort -t ',' -k2,2nr leaderboard.txt
 fi
-if [[ "$stat"=="loss" ]]; then
-   sort -t ',' -k3,3 leaderboard.txt
+if [[ "$stat" == "loss" ]]; then
+   sort -t ',' -k3,3nr leaderboard.txt
  fi
-if [[ "$stat"=="wlr" ]]; then
-   sort -t ',' -k4,4 -k2,2r leaderboard.txt
+if [[ "$stat" == "wlr" ]]; then
+   sort -t ',' -k4,4nr -k2,2n leaderboard.txt
 fi
 done
 	
