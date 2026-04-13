bda77ca88281faee38614089005d2f01f1d5daa9
sathwik <as.28052008@gmail.com>
2026-04-13 19:04:15 +0530

    othello also completed linked game.py and all games but still common classes should be on game.py(not done due to errors)

--- games/game.py
+++ games/game.py
@@ -6,21 +6,129 @@ import sys
 player1=sys.argv[1]
 player2=sys.argv[2]
 class Game:
-    def __init__(self, p1, p2, sym1, sym2, n):
+    def __init__(self, p1, p2, sym1, sym2, n,c):
         self.p1 = p1
         self.p2 = p2
         self.sym1 = sym1
         self.sym2 = sym2
         self.n = np.zeros((n, n), dtype=int)
-        self.state = 0   
+        self.state = 0  
+        self.c = c
+        self.a=n
 
     def turn(self):
         current = self.state
         self.state = 1 - self.state
         return current
 
-    
-        
+    def wincond(self):
+        #horizontal
+        for i in range(self.a):
+            for j in range((self.a-self.c)+1):
+                countx=0
+                counto=0
+                for k in range(self.c):
+                    if self.n[i][j+k]==1:
+                        countx+=1
+                    if self.n[i][j+k]==2:
+                        counto+=1
+                if countx==self.c:
+                    pygame.draw.line(screen,(255,255,255),x_rect[i][j].center,x_rect[i][j+self.c-1].center,5)
+                    font=pygame.font.Font(None,100)
+                    self.text=font.render(f"{self.p1} WON", False,(0,255,255))
+                    self.text_rect=self.text.get_rect(center=(400,400))
+                    return True
+                if counto==self.c:
+                    pygame.draw.line(screen,(255,255,255),x_rect[i][j].center,x_rect[i][j+self.c-1].center,5)
+                    font=pygame.font.Font(None,100)
+                    self.text=font.render(f"{self.p2} WON", False,(0,255,255))
+                    self.text_rect=self.text.get_rect(center=(400,400))
+                    return True
+
+        #vertical
+        for j in range(self.a):
+            for i in range((self.a-self.c)+1):
+                countx=0
+                counto=0
+                for k in range(self.c):
+                    if self.n[i+k][j]==1:
+                        countx+=1
+                    if self.n[i+k][j]==2:
+                        counto+=1
+                if countx==self.c:
+                    pygame.draw.line(screen,(255,255,255),x_rect[i][j].center,x_rect[i+self.c-1][j].center,5)
+                    font=pygame.font.Font(None,100)
+                    self.text=font.render(f"{self.p1} WON", False,(0,255,255))
+                    self.text_rect=self.text.get_rect(center=(400,400))
+                    return True
+                if counto==self.c:
+                    pygame.draw.line(screen,(255,255,255),x_rect[i][j].center,x_rect[i+self.c-1][j].center,5)
+                    font=pygame.font.Font(None,100)
+                    self.text=font.render(f"{self.p2} WON", False,(0,255,255))
+                    self.text_rect=self.text.get_rect(center=(400,400))
+                    return True
+
+        #diagonal 
+        for i in range(self.a - self.c + 1):
+            for j in range(self.a - self.c + 1):
+                countx=0
+                counto=0
+                for k in range(self.c):
+                    if self.n[i+k][j+k]==1:
+                        countx+=1
+                    if self.n[i+k][j+k]==2:
+                        counto+=1
+
+                if countx==self.c:
+                    pygame.draw.line(screen,(255,255,255),x_rect[i][j].center,x_rect[i+self.c-1][j+self.c-1].center,5)
+                    font=pygame.font.Font(None,100)
+                    self.text=font.render(f"{self.p1} WON", False,(0,255,255))
+                    self.text_rect=self.text.get_rect(center=(400,400))
+                    return True
+
+                if counto==self.c:
+                    pygame.draw.line(screen,(255,255,255),x_rect[i][j].center,x_rect[i+self.c-1][j+self.c-1].center,5)
+                    font=pygame.font.Font(None,100)
+                    self.text=font.render(f"{self.p2} WON", False,(0,255,255))
+                    self.text_rect=self.text.get_rect(center=(400,400))
+                    return True
+
+        #diagonal 
+        for i in range(self.a - self.c + 1):
+            for j in range(self.c - 1, self.a):
+                countx=0
+                counto=0
+                for k in range(self.c):
+                    if self.n[i+k][j-k]==1:
+                        countx+=1
+                    if self.n[i+k][j-k]==2:
+                        counto+=1
+
+                if countx==self.c:
+                    pygame.draw.line(screen,(255,255,255),x_rect[i][j].center,x_rect[i+self.c-1][j-(self.c-1)].center,5)
+                    font=pygame.font.Font(None,100)
+                    self.text=font.render(f"{self.p1} WON", False,(0,255,255))
+                    self.text_rect=self.text.get_rect(center=(400,400))
+                    return True
+
+                if counto==self.c:
+                    pygame.draw.line(screen,(255,255,255),x_rect[i][j].center,x_rect[i+self.c-1][j-(self.c-1)].center,5)
+                    font=pygame.font.Font(None,100)
+                    self.text=font.render(f"{self.p2} WON", False,(0,255,255))
+                    self.text_rect=self.text.get_rect(center=(400,400))
+                    return True
+
+        #tie
+        for i in range(self.a):
+            for j in range(self.a):
+                if self.n[i][j]==0:
+                    return False
+
+        print("tie") 
+        font=pygame.font.Font(None,100)
+        self.text=font.render("TIE", False,(0,255,255))
+        self.text_rect=self.text.get_rect(center=(400,400))
+        return True
 
 pg.init()
 screen=pg.display.set_mode((1000,800))
@@ -53,11 +161,11 @@ while True:
                 pg.quit()
                 exit()
             if oth_rect.collidepoint(mouse_pos): 
-                #subprocess.run(["py -3.11","othello.py",player1,player2])
+                subprocess.run(["py","-3.11","othello.py",player1,player2])
                 pg.quit()
                 exit()
             if c4_rect.collidepoint(mouse_pos): 
-                #subprocess.run(["py -3.11","connect4.py",player1,player2])
+                subprocess.run(["py","-3.11","./connect4.py",player1,player2])
                 pg.quit()
                 exit()
 
